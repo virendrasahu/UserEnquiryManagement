7c38f3be945c669d559db68a065d645ca9488087
Virendra Sahu <virendra2609.vs@gmail.com>
2026-03-05 15:09:17 +0530

    Replace axios with fetch for API calls

--- client/src/Enquiry.jsx
+++ client/src/Enquiry.jsx
@@ -16,7 +16,7 @@ export default function Enquiry() {
   })
 
   const getAllEnquiry = () => {
-    axios.get('${import.meta.env.VITE_API_URL}/api/enquiry/list')
+    fetch(`${import.meta.env.VITE_API_URL}/api/enquiry/list')
       .then(res => {
         if (res.data && res.data.status === 200) {
           setEnquiryList(res.data.data);
@@ -40,7 +40,7 @@ export default function Enquiry() {
     // }
 
     if (formData._id) {
-      axios.put(`${import.meta.env.VITE_API_URL}/api/enquiry/update/${formData._id}`, formData)
+      fetch(`${import.meta.env.VITE_API_URL}/api/enquiry/update/${formData._id}`, formData)
         .then(() => {
           toast.success('Enquiry Updated successfully');
           setFormData({
@@ -57,7 +57,7 @@ export default function Enquiry() {
           toast.error('Failed to update enquiry');
         })
     }else{
-      axios.post('${import.meta.env.VITE_API_URL}/api/enquiry/insert', formData)
+      fetch(`${import.meta.env.VITE_API_URL}/api/enquiry/insert', formData)
       .then((res) => {
         console.log(res.data);
         toast.success('Enquiry Added successfully');
